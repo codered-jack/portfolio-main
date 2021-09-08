4cb8aaaa09bdfa347c1020375dd8bc1564abefca
codered-jack <sumitnbq@gmail.com>
2021-09-08 21:54:44 +0530

    update2

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
-  { number: 60, text: 'Github Projects'},
-  { number: 1600, text: 'LinkedIn Connections', },
-  { number: 100, text: 'Leetcode Problems Solved', },
-  { number: 1, text: 'Star Performer Award', }
+  { number: 65, text: "Github Projects" },
+  { number: 1700, text: "LinkedIn Connections" },
+  { number: 150, text: "Leetcode Problems Solved" },
+  { number: 1, text: "Star Performer Award" },
 ];
 
 const Acomplishments = () => (
   <Section>
-  <SectionTitle>Personal Achievements</SectionTitle>
-  <Boxes>
-    {data.map((card, index) => (
-      <Box key={index}>
-        <BoxNum>{`${card.number}`}</BoxNum>
-        <BoxText>{card.text}</BoxText>
-      </Box>
-    ))}
-  </Boxes>
-  <SectionDivider/>
-</Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    <SectionDivider />
+  </Section>
 );
 
 export default Acomplishments;
