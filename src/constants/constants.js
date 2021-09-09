81d14981531d6dfc20599a0b689dc973a7d9f325
codered-jack <sumitnbq@gmail.com>
2021-09-09 13:55:49 +0530

    url change

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -44,8 +44,8 @@ export const projects = [
       " Users can search, sell, add to cart and checkout their favourite items.The application has six main models — Users, Items, Orders, CartItems, OrderItems, and Roles — all of which are relational and showcase the power of relational GraphQL Queries. Working on Testing...",
     image: "/images/sickfits.gif",
     tags: ["Next", "Jest/RTL", "Apollo Client", "Keystone.js"],
-    source: "https://github.com/codered-jack",
-    visit: "https://github.com/codered-jack",
+    source: "https://github.com/codered-jack/sickfrontend",
+    visit: "https://github.com/codered-jack/sickbackend",
     id: 3,
   },
 ];
