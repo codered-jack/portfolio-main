4935cfa75f2c0585a98308d32a4598284cc16365
codered-jack <sumitnbq@gmail.com>
2022-07-17 19:48:10 +0530

    preload bg image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import About from '../components/About';
-import Achievements from '../components/Achievements';
-import Banner from '../components/Banner';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import MouseCircle from '../components/MouseCircle';
-import Navbar from '../components/Navbar';
-import ScrollToTop from '../components/ScrollToTop';
-import SelectedCard from '../components/SelectedCard';
-import Technology from '../components/Technology';
-import Wave from '../components/Wave';
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import About from "../components/About";
+import Achievements from "../components/Achievements";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import MouseCircle from "../components/MouseCircle";
+import Navbar from "../components/Navbar";
+import ScrollToTop from "../components/ScrollToTop";
+import SelectedCard from "../components/SelectedCard";
+import Technology from "../components/Technology";
+import Wave from "../components/Wave";
 const Home: NextPage = () => {
   const router = useRouter();
   const {
@@ -19,18 +19,19 @@ const Home: NextPage = () => {
   } = router;
 
   return (
-    <div className='cursor-none'>
+    <div className="cursor-none">
       <Head>
         <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preload" as="image" href="/images/bg.webp" />
       </Head>
 
       <MouseCircle />
       <ScrollToTop />
       <Navbar />
       <Header />
-      <div id='top'>
-        <div className='fixed w-full h-screen bg-background-pattern bg-cover bg-no-repeat bg-center' />
+      <div id="top">
+        <div className="fixed w-full h-screen bg-background-pattern bg-cover bg-no-repeat bg-center" />
         <Banner />
         <About />
         <SelectedCard id={id} />
